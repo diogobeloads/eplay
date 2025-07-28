a669328e24fc704b4ebf7ccb8082c87f25339064
Diogo Belo <diogobelodss22@gmail.com>
2025-07-28 19:58:22 -0300

    Criação da modal e outros itens.

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,82 @@
+import Section from '../Section'
+
+import { Items, Item, Action, Modal, ModalContent } from './styles'
+
+import resident from '../../assets/images/resident.png'
+import close from '../../assets/images/close.png'
+import spiderman from '../../assets/images/banner-homem-aranha.png'
+import zelda from '../../assets/images/zelda.png'
+import play from '../../assets/images/play.png'
+import zoom from '../../assets/images/zoom.png'
+
+type GalleryItem = {
+  type: 'image' | 'video'
+  url: string
+}
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: zelda
+  },
+  {
+    type: 'image',
+    url: resident
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/snpha1UHU1o?si=wZACSZRD20gITZG1'
+  }
+]
+
+type Props = {
+  defaultCover: string
+  name: string
+}
+
+const Gallery = ({ defaultCover, name }: Props) => {
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GalleryItem) => {
+    if (item.type === 'image') return zoom
+    return play
+  }
+
+  return (
+    <>
+      <Section title="Galeria" background="black">
+        <Items>
+          {mock.map((media, index) => (
+            <Item key={media.url}>
+              <img
+                src={getMediaCover(media)}
+                alt={`Midia ${index + 1} de ${name}`}
+              />
+              <Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Clique para dar play no video"
+                />
+              </Action>
+            </Item>
+          ))}
+        </Items>
+      </Section>
+      <Modal>
+        <ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img src={close} alt="icone fechar" />
+          </header>
+          <img src={spiderman} />
+        </ModalContent>
+        <div className="overlay"></div>
+      </Modal>
+    </>
+  )
+}
+
+export default Gallery
