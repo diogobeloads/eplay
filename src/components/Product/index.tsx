3796dfb30b176be55c334fd8ea30a4e3a33538f6
Diogo Belo <diogobelodss22@gmail.com>
2025-07-28 10:31:00 -0300

    Estilizando os cards e construção das categorias.

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,18 +1,34 @@
 import Tag from '../Tag'
-import { Card, Descricao, Titulo } from './styles'
+import { Card, Descricao, Titulo, Infos } from './styles'
 
-const Product = () => (
+type Props = {
+  title: string
+  category: string
+  system: string
+  description: string
+  infos: string[]
+  image: string
+}
+
+const Product = ({
+  title,
+  category,
+  system,
+  description,
+  infos,
+  image
+}: Props) => (
   <Card>
-    <img src="https://placehold.co/222x250" />
-    <Titulo>Nome do Jogo</Titulo>
-    <Tag>Categoria</Tag>
-    <Tag>Windows</Tag>
-    <Descricao>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi beatae
-      incidunt dignissimos reprehenderit modi! Voluptas pariatur aliquam
-      voluptatum illum, praesentium aspernatur placeat incidunt commodi
-      assumenda nisi, modi magnam? Esse, excepturi.
-    </Descricao>
+    <img src={image} alt={title} />
+    <Infos>
+      {infos.map((info) => (
+        <Tag key={info}>{info}</Tag>
+      ))}
+    </Infos>
+    <Titulo>{title}</Titulo>
+    <Tag>{category}</Tag>
+    <Tag>{system}</Tag>
+    <Descricao>{description}</Descricao>
   </Card>
 )
 
