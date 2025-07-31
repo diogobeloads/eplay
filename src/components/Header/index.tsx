e86ade1c0d1d407eab2156240025eb5f49a8d020
Diogo Belo <diogobelodss22@gmail.com>
2025-07-30 22:39:41 -0300

    Finalizando o carrinho, adição e remoção dos itens.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,35 +1,47 @@
 import { Link } from 'react-router-dom'
 
-import { HeaderBar, Links, LinkItem, LinkCart } from './styles'
+import { HeaderBar, Links, LinkItem, CartButton } from './styles'
 
 import logo from '../../assets/images/logo.svg'
 import carrinho from '../../assets/images/carrinho.svg'
+import { open } from '../../store/reducers/cart'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
 
-const Header = () => (
-  <HeaderBar>
-    <div>
-      <img src={logo} alt="Logo Eplay" />
-      <nav>
-        <Links>
-          <LinkItem>
-            <Link to="/#">Início</Link>
-          </LinkItem>
-          <LinkItem>
-            <Link to="/categories">Categorias</Link>
-          </LinkItem>
-          <LinkItem>
-            <a href="#">Novidades</a>
-          </LinkItem>
-          <LinkItem>
-            <a href="#">Promoções</a>
-          </LinkItem>
-        </Links>
-      </nav>
-    </div>
-    <LinkCart href="#">
-      0 - produto(s)
-      <img src={carrinho} alt="carrinho" />
-    </LinkCart>
-  </HeaderBar>
-)
+const Header = () => {
+  const dispatch = useDispatch()
+  const { items } = useSelector((state: RootReducer) => state.cart)
+
+  const openCart = () => {
+    dispatch(open())
+  }
+
+  return (
+    <HeaderBar>
+      <div>
+        <img src={logo} alt="Logo Eplay" />
+        <nav>
+          <Links>
+            <LinkItem>
+              <Link to="/#">Início</Link>
+            </LinkItem>
+            <LinkItem>
+              <Link to="/categories">Categorias</Link>
+            </LinkItem>
+            <LinkItem>
+              <a href="#">Novidades</a>
+            </LinkItem>
+            <LinkItem>
+              <a href="#">Promoções</a>
+            </LinkItem>
+          </Links>
+        </nav>
+      </div>
+      <CartButton onClick={openCart}>
+        {items.length} - produto(s)
+        <img src={carrinho} alt="carrinho" />
+      </CartButton>
+    </HeaderBar>
+  )
+}
 export default Header
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,41 +4,54 @@ import Tag from '../Tag'
 
 import { Banner, Infos } from './styles'
 import { formataPreco } from '../ProductsList'
+import { useDispatch } from 'react-redux'
+import { add, open } from '../../store/reducers/cart'
+
 type Props = {
   game: Game
 }
 
-const Hero = ({ game }: Props) => (
-  <Banner style={{ backgroundImage: `url(${game.media.cover})` }}>
-    <div className="container">
-      <div>
-        <Tag>{game.details.category}</Tag>
-        <Tag>{game.details.system}</Tag>
-      </div>
-      <Infos>
-        <h2>{game.name}</h2>
-        <p>
-          {game.prices.discount && (
-            <span>De {formataPreco(game.prices.old)}</span>
-          )}
+const Hero = ({ game }: Props) => {
+  const dispatch = useDispatch()
+
+  const addToCart = () => {
+    dispatch(add(game))
+    dispatch(open())
+  }
+
+  return (
+    <Banner style={{ backgroundImage: `url(${game.media.cover})` }}>
+      <div className="container">
+        <div>
+          <Tag>{game.details.category}</Tag>
+          <Tag>{game.details.system}</Tag>
+        </div>
+        <Infos>
+          <h2>{game.name}</h2>
+          <p>
+            {game.prices.discount && (
+              <span>De {formataPreco(game.prices.old)}</span>
+            )}
+            {game.prices.current && (
+              <>
+                Por apenas <span>{formataPreco(game.prices.current)}</span>
+              </>
+            )}
+          </p>
           {game.prices.current && (
-            <>
-              Por apenas <span>{formataPreco(game.prices.current)}</span>
-            </>
+            <Button
+              variant="primary"
+              type="button"
+              title="clique aqui para adicionar"
+              onClick={addToCart}
+            >
+              Adicionar ao carrinho
+            </Button>
           )}
-        </p>
-        {game.prices.current && (
-          <Button
-            variant="primary"
-            type="button"
-            title="clique aqui para adicionar"
-          >
-            Adicionar ao carrinho
-          </Button>
-        )}
-      </Infos>
-    </div>
-  </Banner>
-)
+        </Infos>
+      </div>
+    </Banner>
+  )
+}
 
 export default Hero
